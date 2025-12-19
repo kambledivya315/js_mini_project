628e4c896dc5992734fd68c09e46e6bd478a1afa
kambledivya315 <kambledivya315@gmail.com>
2025-12-19 21:21:20 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,19 @@
+let span=document.querySelector("span");
+let inp=document.querySelector("input");
+let span2=document.querySelector("#span2")
+
+inp.addEventListener("input",function(des){
+  span.textContent=  inp.value.length;
+});
+
+inp.addEventListener("input",function(des){
+
+  let a = 20- inp.value.length;
+  span2.textContent=a;
+  if(a<0){
+    span2.style.color="red";
+  }
+  if(a>=0){
+    span2.style.color="black";
+  }
+});
